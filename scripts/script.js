50e0ac70b5ccf1b6f9f8c73916f5401a93419573
[ayat] <[ahashish@it-blocks.com]>
2024-05-14 10:31:17 +0300

    some change

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 
 $(document).ready(function () {
   
+
      setTimeout(function () {
         $('body').addClass('loaded');
         AOS.init({
@@ -55,7 +56,7 @@ $(document).ready(function () {
             dots: true,
             slideSpeed: 10000,
             autoplaySpeed: 800,
-            autoplayTimeout: 10000,
+            autoplayTimeout: 5000,
             autoplay: true,
             navText: [
                 '<i class="fi-circle-arrow-left1"></i>',
@@ -65,20 +66,27 @@ $(document).ready(function () {
             responsive: {
                 1200: { items: 3 },
                 900: { items: 2 },
-                700: { items: 1 },
-                0: { items: 1 }
+                700: { 
+                    items: 1 ,
+                    center: true 
+
+
+
+                },
+                0: { items: 1 , center: true  }
             }
         })
         $('.outsourcing-sec').owlCarousel({
             loop: true,
             nav: true,
-            margin: 50,
-            items: 3,
+            margin: 20,
+            items: 1,
             dots: true,
             slideSpeed: 10000,
             autoplaySpeed: 800,
             autoplayTimeout: 10000,
             autoplay: true,
+            
             navText: [
                 '<i class="fi-circle-arrow-left1"></i>',
                 '<i class="fi-circle-arrow-right1"></i>'
@@ -91,6 +99,60 @@ $(document).ready(function () {
                 0: { items: 1 }
             }
         })
+
+      
+
+        $('.acquainted-sec').owlCarousel({
+            loop: true,
+            nav: true,
+            margin: 10,
+            items: 4,
+            dots: true,
+            slideSpeed: 10000,
+            autoplaySpeed: 800,
+            autoplayTimeout: 10000,
+            autoplay: true,
+            navText: [
+                '<i class="fi-circle-arrow-left1"></i>',
+                '<i class="fi-circle-arrow-right1"></i>'
+            ],
+            responsiveClass: true,
+            responsive: {
+                1200: { items: 4 },
+                900: { 
+                    items: 3,
+                    margin: 30,
+
+                },
+                700: { items: 2 },
+                0: { items: 1 }
+            }
+        })
+
+
+        $('.have-to-Say-sec').owlCarousel({
+            loop: true,
+            nav: true,
+            margin: 40,
+            items:3,
+            dots: true,
+            slideSpeed: 10000,
+            autoplaySpeed: 800,
+            autoplayTimeout: 10000,
+            autoplay: true,
+            navText: [
+                '<i class="fi-circle-arrow-left1"></i>',
+                '<i class="fi-circle-arrow-right1"></i>'
+            ],
+            responsiveClass: true,
+            responsive: {
+                1200: { items: 3 },
+                900: { items: 3 },
+                700: { items: 1 },
+                0: { items: 1 }
+            }
+        })
+
 
 
         $('.software-specific').owlCarousel({
@@ -216,3 +278,22 @@ $(document).ready(function () {
         nav.classList.toggle("trans")
     })
     close.addEventListener("click", function () { nav.classList.toggle("trans") })
+
+
+
+
+
+    
+    $(document).ready(function() {
+        var navOffset = $('header').offset().top; // الحصول على موضع شريط التنقل الأصلي
+      
+        $(window).scroll(function() {
+          var scrollPos = $(window).scrollTop(); // الحصول على موضع التمرير الحالي
+      
+          if (scrollPos > navOffset) {
+            $('header').addClass('fixed-nav'); // إضافة فئة CSS لتثبيت شريط التنقل
+          } else {
+            $('header').removeClass('fixed-nav'); // إزالة فئة CSS لإلغاء تثبيت شريط التنقل
+          }
+        });
+      });
