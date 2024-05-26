25d40323584c6ae23affd1012c2662843fb769e0
[ayat] <[ahashish@it-blocks.com]>
2024-05-26 14:17:04 +0300

    home

--- scripts/script.js
+++ scripts/script.js
@@ -270,36 +270,71 @@ $(document).ready(function () {
      });  
 
 
- let btn = document.querySelector(".burger-mune")
-    let nav = document.querySelector(".editnav")
-    let close = document.querySelector(".editnav .close")
+//  let btn = document.querySelector(".burger-mune")
+//     let nav = document.querySelector(".editnav")
+//     let close = document.querySelector(".editnav .close")
 
-    btn.addEventListener("click", function () {
-        nav.classList.toggle("trans")
-    })
-    close.addEventListener("click", function () { nav.classList.toggle("trans") })
+//     btn.addEventListener("click", function () {
+//         nav.classList.toggle("trans")
+//     })
+//     close.addEventListener("click", function () { nav.classList.toggle("trans") })
 
 
 
 
 
     
-    $(document).ready(function() {
-        var navOffset = $('header').offset().top; // الحصول على موضع شريط التنقل الأصلي
+//     $(document).ready(function() {
+//         var navOffset = $('header').offset().top; // الحصول على موضع شريط التنقل الأصلي
       
-        $(window).scroll(function() {
-          var scrollPos = $(window).scrollTop(); // الحصول على موضع التمرير الحالي
+//         $(window).scroll(function() {
+//           var scrollPos = $(window).scrollTop(); // الحصول على موضع التمرير الحالي
       
-          if (scrollPos > navOffset) {
-            $('header').addClass('fixed-nav'); // إضافة فئة CSS لتثبيت شريط التنقل
-          } else {
-            $('header').removeClass('fixed-nav'); // إزالة فئة CSS لإلغاء تثبيت شريط التنقل
-          }
-        });
-      });
+//           if (scrollPos > navOffset) {
+//             $('header').addClass('fixed-nav'); // إضافة فئة CSS لتثبيت شريط التنقل
+//           } else {
+//             $('header').removeClass('fixed-nav'); // إزالة فئة CSS لإلغاء تثبيت شريط التنقل
+//           }
+//         });
+//       });
 
-// scrollToTop
+// // scrollToTop
 
-function scrollToTop() {
-    $(window).scrollTop(0);
-}
+// function scrollToTop() {
+
+//     $(window).scrollTop(0);
+// }
+
+
+  var basicScrollTop = function () {  
+    // The button
+    var btnTop = document.querySelector('#goTop');
+
+    // Reveal the button
+    var btnReveal = function () { 
+        if (window.scrollY >= 300) {
+            btnTop.classList.add('is-visible');
+        } else {
+            btnTop.classList.remove('is-visible');
+        }    
+    };  
+
+    // Smooth scroll top
+    var TopscrollTo = function () {
+        var scrollStep = -window.scrollY / 10;
+        var scrollInterval = setInterval(function() {
+            if (window.scrollY != 0) {
+                window.scrollBy(0, scrollStep);
+            } else {
+                clearInterval(scrollInterval);
+            }
+        }, 10);
+    };
+
+    // Listeners
+    window.addEventListener('scroll', btnReveal);
+    btnTop.addEventListener('click', TopscrollTo);  
+};
+
+basicScrollTop();
+  
